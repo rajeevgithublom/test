66ea2009268a15b35621f358678a0beaaee05259
Rajeev Singh <rjvsingh88@gmail.com>
2023-04-21 14:04:07 +0530

    changes to spring jwt main file

---
+++ SpringBootJwtProject/src/main/java/com/icicilombard/jwtauth/SpringBoottestProjectApplication.java
@@ -0,0 +1,16 @@
+package com.icicilombard.jwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoottestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoottestProjectApplication.class, args);
+		
+		
+	}
+
+}
+	
